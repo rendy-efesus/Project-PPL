b92d4f0abd5c6ac0583437ee6691286b27667091
Rendy Efesus Purba <rendyefesuspurba@gmail.com>
2026-04-23 20:01:16 +0700

    Update Quote Request

--- sales-quotation/src/pages/QuoteRequest.jsx
+++ sales-quotation/src/pages/QuoteRequest.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import CalendarPanel from "../components/CalendarPanel";
+import { useState } from "react";
+import RequestorModal from "../components/RequestorModal";
 
 function QuoteRequest() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRequestor, setSelectedRequestor] = useState(null); //simpan data requestor terpilih
+
+  const handleSelectRequestor = (requestor) => {
+    setSelectedRequestor(requestor);
+    setIsModalOpen(false); // Tutup modal setelah pilih
+  };
+
+  const [tripType, setTripType] = useState('One Way or Multi Leg'); // Default value
+
   return (
     <div className='min-h-screen bg-slate-50'>
+      <RequestorModal 
+        isOpen={isModalOpen} 
+        onClose={() => setIsModalOpen(false)} 
+        onSelect={handleSelectRequestor}
+      />
       <div className="px-2 py-2">
     <h1 className="text-slate-800 font-bold text-xl">
       Add New Quote Request
@@ -19,8 +36,9 @@ function QuoteRequest() {
               {/* Requested By */}
               <label className='text-md font-medium'>Requested By:</label>
               <div className='flex w-full md:w-64 border-2 border-slate-300 rounded-md overflow-hidden focus-within:border-blue-500'>
-                <input type="text" className="w-full outline-none px-2 py-1" readOnly />
-                <button type="button" className="p-2 bg-slate-100 border-l border-slate-300 hover:bg-slate-200 text-slate-600">
+                <input type="text" className="w-full outline-none px-2 py-1" readOnly placeholder="Select Customer..."
+                      value={selectedRequestor ? selectedRequestor.contact_person : ''}/>
+                <button type="button" onClick={() => setIsModalOpen(true)} className="p-2 bg-slate-100 border-l border-slate-300 hover:bg-slate-200 text-slate-600">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
                 </button>
               </div>
@@ -40,10 +58,32 @@ function QuoteRequest() {
 
               {/* Trip Type */}
               <label className='text-md font-medium'>Trip Type:</label>
-              <div className='relative w-full md:w-64 group'>
-                <input type="text" className="w-full border-2 border-slate-300 rounded-md focus:border-blue-500 outline-none px-2 py-1 pr-10" readOnly />
+              <div className='relative w-full md:w-64'>
+                <select 
+                  className="w-full border-2 border-slate-300 rounded-md focus:border-blue-500 outline-none px-2 py-1.5 appearance-none bg-white cursor-pointer"
+                  value={tripType}
+                  onChange={(e) => setTripType(e.target.value)}
+                >
+                  <option value="One Way or Multi Leg">One Way or Multi Leg</option>
+                  <option value="Round Trip">Round Trip</option>
+                </select>
+                
+                {/* Icon Panah Custom (untuk menggantikan appearance-none) */}
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-slate-400"><path d="m7 15 5 5 5-5"/><path d="m7 9 5-5 5 5"/></svg>
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="16" 
+                    height="16" 
+                    viewBox="0 0 24 24" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    strokeWidth="2" 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round" 
+                    className="text-slate-400"
+                  >
+                    <path d="m7 15 5 5 5-5"/><path d="m7 9 5-5 5 5"/>
+                  </svg>
                 </div>
               </div>
             </div>
